75e0deb38f04554fb9539c268666fdf2161c82e0
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2023-11-28 13:09:30 -0300

    Update main.js - Aula 3
    ---

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-function tocaSons () {
-
-    if (listaDeTeclas[0].onclick = true) {
-        document.querySelector('#som_tecla_pom').play();
-    } 
-    if (listaDeTeclas[1].onclick = true) {
-        document.querySelector('#som_tecla_clap').play();
-    }
-    
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 }
 
-listaDeTeclas.onclick = tocaSons;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
